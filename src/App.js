fe63e845d60885241a9a538bd7da6c20bfc0415f
Rakib665 <islammdr44@gmail.com>
2022-05-15 02:13:48 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import { Route, Routes } from 'react-router-dom';
+import './App.css';
+import About from './Pages/About/About';
+import Home from './Pages/Home/Home';
+import Navbar from './Pages/Navbar/Navbar';
+
+function App() {
+  return (
+    <div>
+     
+<Navbar></Navbar>    
+<Routes>
+  <Route path='/' element={<Home></Home>}></Route>           
+  <Route path='/about' element={<About></About>}></Route>           
+</Routes>
+
+    </div>
+  );
+}
+
+export default App;
